0eee36667841fce0c4f3dda2d2c243d11af78e41
ElkynSantos <elkynmendoza@unitec.edu>
2023-07-20 11:22:07 -0600

    ComandoParabd

--- APLICACIONASPRMVC/Program.cs
+++ APLICACIONASPRMVC/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//Scaffold - DbContext "Server=LAPTOPELKYN7; Database=ABCDataBase; Trusted_Connection=True; trustServerCertificate=true;" Microsoft.EntityFrameworkCore.SqlServer - OutputDir Models / Db
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AbcdataBaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ABCDataBase")));
 var app = builder.Build();
